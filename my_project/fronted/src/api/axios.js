80f20e6427234e7d492c5b329e3ff34a357dc25f
Manav378 <manupatel5814@gmail.com>
2026-06-28 16:36:30 +0530

    fix: hardcode fallback API URL

--- my_project/fronted/src/api/axios.js
+++ my_project/fronted/src/api/axios.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_API_URL ||'https://documind-backend-posd.onrender.com/api',
     withCredentials:true
 })
 
+
+console.log('API URL:', import.meta.env.VITE_API_URL)
 export default api
